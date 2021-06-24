1e892768a7f24460a6d6378769e332c661a67e0b
Đinh Trọng Đạt <71766267+Dat0309@users.noreply.github.com>
2021-06-24 16:50:04 +0700

    Update training_cnn.py

--- training_cnn.py
+++ training_cnn.py
@@ -9,12 +9,14 @@ from keras.callbacks import ModelCheckpoint
 from numpy.core.defchararray import mod
 from sklearn.model_selection import train_test_split
 from sklearn.utils import validation
+from matplotlib import pyplot as plt
 
 data = np.load('data.npy')
 target = np.load('target.npy')
 
 model = Sequential()
 
+#Trích lọc đặc trưng của ảnh
 #Lớp CNN đầu tiên, theo sau là các lớp Relu và MaxPooling
 model.add(Conv2D(200,(3,3),input_shape = data.shape[1:]))
 model.add(Activation('relu'))
@@ -41,3 +43,20 @@ train_data, test_data, train_target, test_target = train_test_split(data,target,
 
 checkpoint = ModelCheckpoint('model-{epoch:03d}.model',monitor='val_loss',verbose=0,save_best_only=True, mode = 'auto')
 history = model.fit(train_data,train_target,epochs=20, callbacks=[checkpoint],validation_split=0.2)
+
+plt.plot(history.history['loss'], 'r', label = 'training loss')
+plt.plot(history.history['val_loss'], label = 'validation loss')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+
+plt.plot(history.history['accuracy'],'r',label='training accuracy')
+plt.plot(history.history['val_accuracy'],label='validation accuracy')
+plt.xlabel('# epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+print(model.evaluate(test_data,test_target))
